79eb1a65ed3a25b3c2de0013c6ec5b308071a124
Shravan Kumar S Bagewadi <shravansb1234@gmail.com>
2023-08-04 03:21:08 +0530

    Lambda Terraform Code
    Lambda Terraform Code

--- main.tf
+++ main.tf
@@ -1,23 +1,86 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0ded8326293d3201b"
-  instance_type = "t2.micro"
+resource "aws_iam_role" "lambda_role" {
+  name               = "terraform_aws_lambda_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+# IAM policy for logging from a lambda
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Policy Attachment on the role.
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+# Generates an archive from content, a file, or a directory of files.
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
+}
+
+# Create a lambda function
+# In terraform ${path.module} is the current directory.
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Java-Lambda-Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "com.shravan.lambda.HealthHandler::handleRequest"
+  runtime       = "java17"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+
+output "teraform_aws_role_output" {
+  value = aws_iam_role.lambda_role.name
+}
+
+output "teraform_aws_role_arn_output" {
+  value = aws_iam_role.lambda_role.arn
+}
+
+output "teraform_logging_arn_output" {
+  value = aws_iam_policy.iam_policy_for_lambda.arn
 }
