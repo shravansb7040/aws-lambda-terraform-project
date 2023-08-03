bc38a64c0318ef189c10c4d4c522a6c3ab2bb755
Shravan Kumar S Bagewadi <shravansb1234@gmail.com>
2023-08-03 14:01:39 +0530

    Update AMI ID for ap-south-1 region
    Update AMI ID for ap-south-1 region

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0ded8326293d3201b"
   instance_type = "t2.micro"
 
   tags = {
